d84639bcdcbd44bf7ed4c7236e05008dc5496cd6
ordes <10027@holbertonstudents.com>
2025-04-02 09:56:12 +0000

    added file

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,8 @@
+/*eslint-disable*/
 export default function getListStudents() {
-    return[
-        {id: 1, firstname: 'Guillaume', location: 'San Francisco' },
-        {id: 2, firstname: 'James', location: 'Columbia' },
-        {id: 5, firstname: 'Serena', location: 'San Francisco' }
-    ]
+	return [
+		{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+		{ id: 2, firstName: 'James', location: 'Columbia' },
+		{ id: 5, firstName: 'Serena', location: 'San Francisco' }
+	]
 }
