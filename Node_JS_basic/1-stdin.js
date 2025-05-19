ed09571c625323303e168072f813c1778b0ae67a
ordes <10027@holbertonstudents.com>
2025-05-19 11:29:37 +0000

    added file

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('data', (data)=>{
+    process.stdout.write(`Your name is : ${data}`);
+});
+
+process.stdin.on('close', ()=>{
+    console.log('This important software is now closing');
+    process.exit(0);
+});
