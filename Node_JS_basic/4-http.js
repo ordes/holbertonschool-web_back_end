bcdc6693f0b2d8b1885ef1d84faff907151fa564
ordes <10027@holbertonstudents.com>
2025-05-19 11:44:06 +0000

    added file

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const port = 1245;
+const app = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type' : 'text/plain'});
+});
+
+app.listen(port);
+module.export = app;
