c9ed2659528b9f8ee0f1963ce647d7aaabc72995
ordes <10027@holbertonstudents.com>
2025-05-19 11:31:03 +0000

    added file

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,14 @@
-/* eslint-disable */
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', (data)=>{
-    process.stdout.write(`Your name is : ${data}`);
+
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', () => {
+  const inputData = process.stdin.read();
+  if (inputData !== null) {
+    process.stdout.write(`Your name is: ${inputData}`);
+  }
 });
 
-process.stdin.on('close', ()=>{
-    console.log('This important software is now closing');
-    process.exit(0);
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
