ef317f59e2a687b2784d28532376d41e98eaba61
ordes <10027@holbertonstudents.com>
2025-05-20 10:18:44 +0000

    added file

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,10 +1,10 @@
-/* eslint-disable */
 const http = require('http');
 
 const port = 1245;
-const app = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type' : 'text/plain'});
-});
+const app = http.createServer((req, res) => (
+  res.writeHead(200, { 'Content-Type': 'text/plain'});
+  res.end('Hello Holberton School!');
+));
 
 app.listen(port);
-module.export = app;
+module.exports = app;
